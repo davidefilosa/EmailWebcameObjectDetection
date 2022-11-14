012c528e5e37d0270f4b2b0d4fd079694f123539
Davide Filosa <davideclaypool@gmail.com!>
2022-11-14 10:25:59 +0100

    Added video capture functionality

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import cv2
+import time
+
+video = cv2.VideoCapture(0)
+time.sleep(1)
+
+while True:
+    check, frame = video.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+    cv2.imshow('My Video', gray_frame_gau)
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+video.release()
+
+
+
 
